cf7da45f9ab9316ca8324f1179bb7e261ce5b125
Troels Liebe Bentsen <tlb@rapanden.dk>
2017-03-06 10:57:15 +0100

    Make echo service

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ var io = require('socket.io')(server);
 
 app.use(express.static("./public"));
 
-io.on('connection', function(){
-
+io.on('connection', (client) => {
+    client.on('event', (data) => {
+        console.log(`From client: ${data}`)
+        client.emit("Reply:" + data);
+    });
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log("Listening on http://localhost:3000/");
+});
