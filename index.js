15bda957c88b74329083cfa846d2cfb6cb08952b
Troels Liebe Bentsen <tlb@rapanden.dk>
2017-03-06 10:31:52 +0100

    Add public folder

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(){ /* … */ });
+app.use(express.static("./public"));
+
+io.on('connection', function(){
+
+});
 
 server.listen(3000);
